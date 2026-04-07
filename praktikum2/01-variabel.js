07b87e098d056fe13187d61a3696f19549e0bd5f
mifonly10-spec <mifonly10@gmail.com>
2026-04-07 23:00:09 +0700

    update

---
+++ praktikum2/01-variabel.js
@@ -0,0 +1,17 @@
+let nama = 'budi';
+let umur =20;
+let kota ='banda aceh';
+
+console.log('=== data mahasiswa ===');
+console.log('nama:',nama);
+console.log('umur:',umur);
+console.log('kota:',kota);
+
+umur =21;
+console.log('ulang tahun! umur sekarang:', umur);
+
+const jurusan ='teknik informatika';
+const tahunmasuk = 2023;
+
+console.log('jurusan   :',jurusan);
+console.log('tahun masuk:',tahunmasuk);
